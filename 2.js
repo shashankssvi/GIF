263ec157ad3368c71f58705bbe354231fa790378
shashank.s <148774696+shashankssvi@users.noreply.github.com>
2024-07-08 19:33:39 +0530

    GIF

--- 2.js
+++ 2.js
@@ -11,6 +11,20 @@ ExpLink=[]
 Image_Link=[]
 About=[]
 var value1=""
+
+function calc(lat1,lat2,log1,log2){
+    const R = 6371;
+    const φ1 = lat1 * Math.PI/180;
+    const φ2 = lat2 * Math.PI/180;
+    const Δφ = (lat2-lat1) * Math.PI/180;
+    const Δλ = (log2-log1) * Math.PI/180;
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+              Math.cos(φ1) * Math.cos(φ2) *
+              Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    const d = R * c;
+    return d;
+}
 fetch('4.json')
 .then(response => response.json())
 .then(data1 => {
@@ -28,18 +42,28 @@ fetch('4.json')
     }
 })
 .then(() => {
+    let dist={};
+    len=ExpLink.length;
+    for(let i=0;i<len;i++){
+        l1=myf(String(ExpLink[i]));
+        v1=calc(l1[0],latitude,l1[1],longitude);
+        dist[i]=v1;
+    }
+    const sortedEntries = Object.entries(dist).sort(([, a], [, b]) => a - b);
+    const sortedKeys = sortedEntries.map(([key]) => parseInt(key));
+    console.log("Sorted Keys:", sortedKeys);
     dev=document.getElementById("div");
-    Name1.forEach(function(element){
     var p = document.createElement("p");
     var button = document.createElement("button");
-    button.textContent=element;
+    button.textContent=Name1[sortedKeys[0]];
+    
     button.onclick=function(){
         value1=button.innerText;
-        myfun1();
-    };   
+        myfun1(sortedKeys[0]);
+    };
     p.appendChild(button);
     dev.appendChild(p);
-    });
+    myfun1(sortedKeys[0]);
 })
 .catch(error => {
     console.error('Error fetching JSON:', error);
@@ -48,6 +72,7 @@ fetch('4.json')
 // console.log(Name1);
 let latitude=sessionStorage.getItem('lat1');
 let longitude=sessionStorage.getItem('lon1');
+
 function latlog(){
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -65,15 +90,16 @@ if (navigator.geolocation) {
 }
 
 latlog();
-let sc;
-async function myfun(searchby) {
-    sc=searchby;
+var val=document.getElementsByClassName("sel").value;
+// document.getElementById("1").innerHTML=val;
+console.log(val)
+searchby=State;
+async function myfun() {
     let sce = document.getElementById("search").value.toLowerCase();
     let dev1 = document.getElementById("div");
     let arr1 = searchby.filter(function(element) {
         return element.toLowerCase().includes(sce);
     });
-    console.log(searchby);
     dev1.innerHTML = "";
     arr1.forEach(function(element) {
         var p = document.createElement("p");
@@ -88,28 +114,22 @@ async function myfun(searchby) {
     });
 }
 
-// document.getElementById("search").addEventListener("input", myfun(sc));
+document.getElementById("search").addEventListener("input", myfun);
 
 // myfun();
 
-function calc(lat1,lat2,log1,log2){
-    const R = 6371;
-    const φ1 = lat1 * Math.PI/180;
-    const φ2 = lat2 * Math.PI/180;
-    const Δφ = (lat2-lat1) * Math.PI/180;
-    const Δλ = (log2-log1) * Math.PI/180;
-    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-              Math.cos(φ1) * Math.cos(φ2) *
-              Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    const d = R * c;
-    return d;
-    }
-    
+function myf(exp){
+    a=String(exp).split("@")
+    console.log(a);
+    list2=a[0].replace("https://www.google.com/maps/place/","")
+    list1=a[1].split(",")
+    return list1;
+}
 
-function myfun1(){
+
+
+function myfun1(ind){
     dev = document.getElementById('div3');
-    ind=Name1.indexOf(value1);
     console.log(ind);
     console.log(ExpLink[ind]);
     a=String(ExpLink[ind]).split("@")
@@ -117,6 +137,8 @@ function myfun1(){
     list2=a[0].replace("https://www.google.com/maps/place/","")
     console.log(Name1[ind]);
     list1=a[1].split(",")
+    console.log(list1);
+
     a=(parseFloat(list1[0])+latitude)/2;
     b=(parseFloat(list1[1])+longitude)/2;
     lis1="https://www.google.com/maps/dir/"+latitude+","+longitude+"/"+list1[0]+","+list1[1]+"/@"+list1[0]+","+list1[1]+",7z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x0:0xd8ef8500652e24c0!2m2!1d74.7849003!2d13.2547441!1m5!1m1!1s0x0:0x226c2048cd0eb4a8!2m2!1d76.6883061!2d9.3285168!3e0?hl=en&entry=ttu";
@@ -145,3 +167,10 @@ function myfun1(){
     
     console.log(calc(latitude,list1[0],longitude,list1[1]));
 }
+/* <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d996492.2751816645!2d47.0793674428846!3d12.677617788649373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m4!1s0x0%3A0xd8ef8500652e24c0!3m2!1d"+latitude+"!2d"+longitude+"!4m4!1s0x0%3A0x226c2048cd0eb4a8!3m2!1d"+list2[1]+"!2d"+list2[1]+"!5e0!3m2!1sen!2sin!4v1715644823340!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */
+/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14497.763612920437!2d78.13807015!3d24.711739199999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3979918b9ac9c1c1%3A0x6ad66abbe3ddb9fb!2sChanderi%2C%20Madhya%20Pradesh%20473446!5e0!3m2!1sen!2sin!4v1715649789400!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */
+{/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7874.109170187392!2d76.6879227!3d9.328431250000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b063cfc4923b5a7%3A0x720304f454797c45!5e0!3m2!1sen!2sin!4v1715650086735!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+9.3284313,76.6879227
+{/* <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3937.0545248837475!2d76.68534777314791!3d9.328436584233803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOcKwMTknNDIuNCJOIDc2wrA0MScxNi41IkU!5e0!3m2!1sen!2sin!4v1715650641441!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+{/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7874.109170187392!2d76.6879227!3d9.328431250000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b063cfc4923b5a7%3A0x720304f454797c45!2sAranmula%2C%20Kerala!5e0!3m2!1sen!2sin!4v1715650851284!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+
