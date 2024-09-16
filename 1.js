1f890ebd118058db3b0324be932a249219dc04c0
shashank.s <148774696+shashankssvi@users.noreply.github.com>
2024-09-17 03:00:32 +0530

    update

--- 1.js
+++ 1.js
@@ -1,51 +1,39 @@
-let list1=[]
-let a=""
-let len;
-Name1=[]
-Application=[]
-Goods=[]
-State=[]
-Production=[]
-Location_Link=[]
-ExpLink=[]
-Image_Link=[]
-About=[]
-var value1=""
-fetch('4.json')
-.then(response => response.json())
-.then(data1 => {
-    len=data1.length;
-    for (let i=0;i<len;i++){
-        Name1.push(String(data1[i]['Name']));
-        Application.push(String(data1[i]["Application Number"]));
-        Goods.push(String(data1[i]["Goods (As per Sec 2 (f) of GIAct 1999 )"]));
-        State.push(String(data1[i]["State"]));
-        Production.push(String(data1[i]["Production Location"]));
-        Location_Link.push(String(data1[i]["Location Link"]));
-        ExpLink.push(String(data1[i]["Exp Link"]));
-        Image_Link.push(String(data1[i]["Image Link"]));
-        About.push(String(data1[i]["About Product"]));
-    }
-})
-.then(() => {
-    dev=document.getElementById("div");
-    Name1.forEach(function(element){
-    var p = document.createElement("p");
-    var button = document.createElement("button");
-    button.textContent=element;
-    button.onclick=function(){
-        value1=button.innerText;
-        myfun1();
-    };   
-    p.appendChild(button);
-    dev.appendChild(p);
-    });
-})
-.catch(error => {
-    console.error('Error fetching JSON:', error);
-});
+var sino = sessionStorage.getItem('Sl.No');
+console.log(sino)
+        function createProductDiv(product) {
+            return `
+                <div class="product">
+                    <h2>Name: ${product["Name"]}</h2>
+                    <h3 class="application-number">Application Number: ${product["Application No."]}</h3>
+                    <h3 class="goods-type">Goods Type: ${product["Goods(As per Sec 2 (f) of GI Act 1999 )"]}</h3>
+                    <h3 class="state">State: ${product["STATE"]}</h3>
+                    <h3 class="production-origin">Production Origin: ${product["PLACE"]}</h3>
+                    <button type="button" onclick="window.location.href='${product["Exp Link"]}'">Location</button>
+                    <button type="button" onclick="window.location.href='${product["IMAGE LINK"]}'">Image</button>
+                    <button type="button" onclick="window.location.href='${product["ABOUT GI"]}'">Description</button>
+                </div>
+            `;
+        }
 
-// console.log(Name1);
+        // Fetch product data and display the filtered product
+        const container1 = document.getElementById('divv1');
+
+        fetch('4.json')
+            .then(response => response.json())
+            .then(data => {
+                // Filter products based on the stored Sl.No
+                const filteredProducts = data.filter(element => element["Sl.No"] === sino);
+
+                console.log(filteredProducts[0])
+                // Append each filtered product to the container
+                filteredProducts.forEach(product => {
+                    container1.innerHTML += createProductDiv(product);
+                    myfun1(product["Exp Link"])
+                });
+
+            })
+            .catch(error => console.error('Error fetching JSON:', error));
+
 let latitude=sessionStorage.getItem('lat1');
 let longitude=sessionStorage.getItem('lon1');
 function latlog(){
@@ -59,6 +47,7 @@ if (navigator.geolocation) {
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 });
 } else {
+    alert("Geolocation is not supported by this browser.")
     console.log("Geolocation is not supported by this browser.");
 }
 
@@ -66,19 +55,24 @@ if (navigator.geolocation) {
 
 latlog();
 let sc;
-async function myfun(searchby) {
-    sc=searchby;
+async function myfun() {
     let sce = document.getElementById("search").value.toLowerCase();
     let dev1 = document.getElementById("div");
     let arr1 = searchby.filter(function(element) {
         return element.toLowerCase().includes(sce);
     });
-    console.log(searchby);
     dev1.innerHTML = "";
+    console.log(all1.filter(function(element){
+        return element;
+    }));
+    
     arr1.forEach(function(element) {
         var p = document.createElement("p");
         var button = document.createElement("button");
         button.textContent=element;
+    button.style.backgroundColor="#FF7722";
+    button.style.color="blue";
+    button.style.fontWeight="bold";
         button.onclick=function(){
         value1=button.innerText;
         myfun1();
@@ -107,15 +101,11 @@ function calc(lat1,lat2,log1,log2){
     }
     
 
-function myfun1(){
-    dev = document.getElementById('div3');
-    ind=Name1.indexOf(value1);
-    console.log(ind);
-    console.log(ExpLink[ind]);
-    a=String(ExpLink[ind]).split("@")
+function myfun1(explink){
+    dev = document.getElementById("page1");
+    a=explink.split("@")
     console.log(a);
     list2=a[0].replace("https://www.google.com/maps/place/","")
-    console.log(Name1[ind]);
     list1=a[1].split(",")
     a=(parseFloat(list1[0])+latitude)/2;
     b=(parseFloat(list1[1])+longitude)/2;
@@ -129,19 +119,6 @@ function myfun1(){
     a.loading = "lazy";
     a.referrerPolicy = "no-referrer-when-downgrade";
     a.src=link2;
-    dev = document.getElementById('tab2');
-    document.getElementById('div3').style.display="table";
-    dev.querySelector("#a1").innerText=Name1[ind];
-    dev.querySelector("#a2").innerText=Application[ind];
-    dev.querySelector("#a3").innerText=Goods[ind];
-    dev.querySelector("#a4").innerText=State[ind];
-    dev.querySelector("#a5").innerText=Production[ind];
-    dev.querySelector("#a6").href=lis1;
-    dev.querySelector("#a6").innerText="Location";
-    dev.querySelector("#a7").href=Image_Link[ind];
-    dev.querySelector("#a7").innerText="Images";
-    dev.querySelector("#a8").href=About[ind];
-    dev.querySelector("#a8").innerText="About";
-    
     console.log(calc(latitude,list1[0],longitude,list1[1]));
 }
+
